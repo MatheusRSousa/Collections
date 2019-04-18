04919220873851046133474de66ff8651c48aaf5
Matheus Rodrigues <matheus.rsousa16@gmail.com>
2019-04-18 20:06:15 -0300

    ArrayList internamente

---
+++ src/Collections1/MeuArrayList.java
@@ -0,0 +1,98 @@
+package Collections1;
+
+
+
+public class MeuArrayList<T> {
+	private static final int TAMANHO_INICIAL = 5;
+	Object[] array = new Object[TAMANHO_INICIAL];
+	private int tamanho;
+	
+	
+	public void add(Object object) {
+		if(tamanho == array.length) {
+			Object[] novoArray = new Object[tamanho+TAMANHO_INICIAL];
+			for (int i = 0; i < array.length; i++) {
+				novoArray[i] = array[i];
+			}
+			array=novoArray;
+		}
+		if(object != null) {
+			array[tamanho] = object;
+			tamanho++;
+		}
+	}
+	
+	public void add(int index, Object object) throws IndexOutOfBoundsException{
+		if(index >= tamanho || index < 0 || tamanho == 0) {
+			throw new IndexOutOfBoundsException("index invalido");
+		}
+		array[index] = object;
+		
+	}
+	
+	public void remove(Object object) {
+		boolean achou = false;
+		if(object != null && tamanho > 0) {
+			int pos = 0;
+			Object[] novoArray = new Object[tamanho];
+			for (int i = 0; i < array.length; i++) {
+				if(array[i] == object) {
+					achou = true;
+					array[i] = null;
+					break;
+				}
+			}
+			if(achou) {
+				for (int i = 0; i < array.length; i++) {
+					if(array[i]!=null) {
+						novoArray[pos] = array[i];
+						pos++;
+					}
+				}
+				tamanho--;
+				array = novoArray;
+			}
+		}
+		
+	}
+	
+	public void remove(int index) throws IndexOutOfBoundsException{
+		if(index >= tamanho || index < 0 || tamanho == 0) {
+			throw new IndexOutOfBoundsException("index invalido");
+		}
+	
+		int pos = 0;
+		Object[] novoArray = new Object[tamanho];
+		array[index] = null;
+		for (int i = 0; i < array.length; i++) {
+			if(array[i]!=null) {
+				novoArray[pos] = array[i];
+				pos++;
+			}
+		}
+		tamanho--;
+		array = novoArray;
+		
+	}
+	
+	public void set(int index, Object object) throws IndexOutOfBoundsException{
+		if(index < 0 || index >= tamanho || tamanho == 0) {
+			throw new IndexOutOfBoundsException("index invalido");
+		}
+		array[index] = object;
+
+	}
+	
+	public Object get(int index) throws IndexOutOfBoundsException{
+		if(index < 0 || index >= tamanho || tamanho == 0) {
+			throw new IndexOutOfBoundsException("index invalido");
+		}
+		return array[index];
+		
+	}
+	
+	public int size() {
+		return tamanho;
+	}
+
+}
---
+++ src/Collections1/MeuArrayListTest.java
@@ -0,0 +1,135 @@
+package Collections1;
+
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+import org.junit.jupiter.api.Test;
+
+class MeuArrayListTest {
+
+
+	@Test
+	void testAdd() {
+		MeuArrayList<Object> array = new MeuArrayList<Object>();
+		array.add("Matheus");
+		assertEquals(1, array.size());
+		array.add("cafe");
+		array.add("bala");
+		array.add("hamurabi");
+		assertEquals("bala", array.get(2));
+		try {
+			array.add(-1, "galinha");
+			assertTrue("não disparou a exception", false);
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue("disparou a exception", true);
+		}
+		array.add(2,"novo");
+		assertEquals("novo", array.get(2));
+	}
+	
+	@Test
+	void testeRemove() {
+		MeuArrayList<Object> array = new MeuArrayList<Object>();
+		try {
+			array.remove(0);
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(true);
+		}
+		array.add("Matheus");
+		array.remove(0);
+		assertEquals(0, array.size());
+		array.add("Matheus1");
+		array.add("cana");
+		array.add("maconha");
+		array.remove("cana");
+		assertEquals("maconha", array.get(1));
+		try {
+			array.remove(-1);
+			assertTrue("Não disparou a exception", false);
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue("Disparou a exception",true);
+		}
+		try {
+			array.remove(2);
+			assertTrue("não disparou a exception",false);
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(true);
+		}
+		
+		
+	}
+	
+	@Test 
+	void testeGet() {
+		MeuArrayList<Object> array = new MeuArrayList<Object>();
+		try {
+			array.get(0);
+			assertTrue(false);
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(true);
+		}
+		array.add("Matheus1");
+		array.add("Matheus2");
+		assertEquals("Matheus2", array.get(1));
+		try {
+			array.get(-1);
+			assertTrue("não disparou a exception", false);
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue("disparou a exception",true);
+		}
+		
+		try {
+			array.get(2);
+			assertTrue(false);
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	void testSet() {
+		MeuArrayList<Object> array = new MeuArrayList<Object>();
+		try {
+			array.set(0, "feijão");
+			assertTrue(false);
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(e.getMessage(),true);
+		}
+		array.add("paçoca");
+		array.add("cuscuz");
+		array.add("bolo");
+		array.set(2, "pao");
+		assertEquals("pao", array.get(2));
+		try {
+			array.set(3, "bolacha");
+			assertTrue(false);
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(e.getMessage(),true);
+		}
+		
+		try {
+			array.set(-1, "carne");
+			assertTrue(false);
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(e.getMessage(), true);
+		}
+	}
+	
+	@Test 
+	void testSize() {
+		MeuArrayList<Object> array = new MeuArrayList<Object>();
+		assertEquals(0, array.size());
+		array.add(1);
+		array.add(2);
+		array.add(3);
+		array.add(4);
+		array.add(5);
+		assertEquals(5, array.size());
+		array.remove(3);
+		assertEquals(4, array.size());
+	}
+
+}
